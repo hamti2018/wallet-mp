39c1e8e377536461b1b35c16c43d1aa10aabe013
Stas Matveev <sm485019@gmail.com>
2021-10-17 20:19:49 +0300

    Update Wallet.jsx

--- src/components/wallet/Wallet.jsx
+++ src/components/wallet/Wallet.jsx
@@ -1,19 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TokenCard } from "./TokenCard";
 import { SendToPool } from "./SendToPool";
+import toast, { Toaster } from 'react-hot-toast';
+
 import "./Wallet.css"
 
 const Wallet = () => {
+    const [message, setMessage] = useState("")           //При каждом изменении message появляется сообщение об ошибке
     const [poolCard, setPoolCard] = useState(false)
+    useEffect(() => {
+        if (message !== "") {
+            toast.error(message, {
+                style: {
+                    fontSize: "14px",
+                    fontWeight: "700",
+                    padding: '16px',
+                    color: 'black',
+                },
+                duration: 6000
+            })
+        }
+    }, [message])
 
     return (
+
         <div className="wrapper">
+            <Toaster />
             <SendToPool
                 poolCard={poolCard}
                 setPoolCard={setPoolCard}
             />
             <div className="wallet">
                 <div className="wallet__header"> Кошелёк</div>
+                <button
+                onClick={() => {
+                    localStorage.removeItem("sc")
+                    window.location.href = 'http://localhost:3000/login';
+                }}
+                className="wallet__exit">
+                    Выход
+                </button>
                 <div className="wallet__cards">
                     <TokenCard
                         name={"MINE"}
